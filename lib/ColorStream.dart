2695da989a6abdbed87f85a513bb850e350fc224
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-11-25 00:07:35 +0700

    W12: Jawaban Soal 2

---
+++ lib/ColorStream.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Colorstream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.black,
+    Colors.white,
+    Colors.red,
+    Colors.indigo,
+    
+  ];
+  
+}
